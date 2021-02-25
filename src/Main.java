28faecc043327966e8a07c9fb7813a6e80ddf948
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2021-02-25 15:21:38 +0300

    first commit

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.Vector;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/Paint.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class Paint extends JFrame {
+
+    public static void main(String[] args) {
+
+    }
+}
